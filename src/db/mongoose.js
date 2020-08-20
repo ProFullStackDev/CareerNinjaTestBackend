838e336b0f87ea64befb2b3d667da3cc1d298706
GodFather93 <suryapalsingh.inda@gmail.com>
2020-08-20 20:00:23 +0530

    removed config

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,5 @@
 const mongoose = require ('mongoose');
 const connectionURL =
-  process.env.MONGODB_URL ||
   'mongodb+srv://Surya:surya007@cluster0-bwfkc.mongodb.net/test?retryWrites=true&w=majority';
 
 mongoose.connect (connectionURL, {
